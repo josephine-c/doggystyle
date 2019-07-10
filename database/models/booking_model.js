73776c54c1ebbac1032dd9cf5a9c67e657f977c0
John <johnrubio93@gmail.com>
2019-07-10 12:36:31 +1000

    created booking schema and model

---
+++ database/models/booking_model.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const BookingSchema = require("./../schemas/booking_schema");
+
+const BookingModel = mongoose.model("Booking", BookingSchema);
+
+module.exports = BookingModel;
