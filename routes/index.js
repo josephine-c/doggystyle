7d66fc4604f42468843ab5528d076ec129d661c7
John <johnrubio93@gmail.com>
2019-07-11 11:37:42 +1000

    minor controller updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const BookingRoutes = require("./booking_routes");
 
 router.get("/", (req, res) => res.send("Welcome"));
-router.use("/booking", BookingRoutes);
+router.use("/bookings", BookingRoutes);
 
 
 module.exports = router;
