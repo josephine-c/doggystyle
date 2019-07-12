79067678054e03c6056cd5d0f20ad725851836b3
Jo <josephine.yt.chong@gmail.com>
2019-07-12 14:58:09 +1000

    Added to routes/index.js ContactRouter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const BookingRoutes = require("./booking_routes");
+const ContactRoutes = require("./contact_routes");
 
 router.get("/", (req, res) => res.send("Welcome"));
 router.use("/bookings", BookingRoutes);
-
+router.use("/contactus", ContactRoutes);
 
 module.exports = router;
