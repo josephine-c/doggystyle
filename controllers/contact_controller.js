a8df4da7dbc96de6ebc1788315153f774ae6ea59
Jo <josephine.yt.chong@gmail.com>
2019-07-18 10:38:30 +1000

    Added email function to enquires so that after you submit an enquiry this sends an email to trainer and enquirer

--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -1,24 +1,58 @@
 const ContactModel = require("./../database/models/contact_model");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        api_key: process.env.EMAIL_KEY
+    }
+}));
 
 //needs to return json to react client or a jwt, authentication can be done with cors and jwt in auth header from client end
 async function index(req, res) {
  const contacts = await ContactModel.find();
  return res.json(contacts);
- // return res.render("test");
+
 }
 
 async function create(req, res) {
- const { name, email, phone, suburb, dogDetails, details } = req.body;
- const contact = await ContactModel.create({
-   name,
-   email,
-   phone,
-   suburb,
-   dogDetails,
-   details
- }).catch(err => res.status(500).send(err));
- 
- console.log("post", contact);
+ const { 
+     name, 
+     email, 
+     phone, 
+     suburb, 
+     dogDetails, 
+     details 
+} = req.body;
+
+ const contact = await ContactModel.create(
+     {
+        name,
+        email,
+        phone,
+        suburb,
+        dogDetails,
+        details
+        }
+ ).catch(err => res.status(500).send(err));
+
+//enquires email sent to admin
+ await transporter.sendMail({
+    to: "josephine.yt.chong@gmail.com",
+    from: email,
+    subject: "New Enquiry/Comment",
+    html: "<h1>An enquiry has been made please reply!</h1>",
+    priority: "high"
+});
+//enquires email sent to user
+await transporter.sendMail({
+    to: email,
+    from: "dog@trainer.com",
+    subject: "Enquiry/Comment has been Sent",
+    html: "<h1>Your enquiry/comment has been sent!</h1>",
+    priority: "high"
+});
+
+ console.log("Message sent", contact);
  // res.redirect("/contact");
  return res.json(contact);
 }
@@ -27,7 +61,6 @@ async function show(req, res) {
  const { id } = req.params;
  const contact = await ContactModel.findById(id);
  console.log(contact);
- // res.render("test");
  return res.json(contact);
 }
 
@@ -43,7 +76,6 @@ async function update(req, res) {
  return res.json(contact);
 }
 
-//function may need fixing
 async function edit(req, res) {
  const { id } = req.params;
  let query = { _id: id };
