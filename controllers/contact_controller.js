3ce817808b528945946d48f262b2c59ec0d30e0b
Nathaniel <nathanielmorales01@gmail.com>
2019-07-22 11:33:14 +1000

    first integration test for creating a contact us form

--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -1,105 +1,102 @@
 const ContactModel = require("./../database/models/contact_model");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
-const transporter = nodemailer.createTransport(sendgridTransport({
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
     auth: {
-        api_key: process.env.EMAIL_KEY
+      api_key: process.env.EMAIL_KEY
     }
-}));
+  })
+);
 
 //needs to return json to react client or a jwt, authentication can be done with cors and jwt in auth header from client end
 async function index(req, res) {
- const contacts = await ContactModel.find();
- return res.json(contacts);
-
+  const contacts = await ContactModel.find();
+  return res.json(contacts);
 }
 
 async function create(req, res) {
- const { 
-     name, 
-     email, 
-     phone, 
-     suburb, 
-     dogDetails, 
-     details 
-} = req.body;
+  const { name, email, phone, suburb, dogDetails, details } = req.body;
 
- const contact = await ContactModel.create(
-     {
-        name,
-        email,
-        phone,
-        suburb,
-        dogDetails,
-        details
-        }
- ).catch(err => console.log("&&&&&&&&&&&", err));
- //.catch(err => res.status(500).send(err));
+  const contact = await ContactModel.create({
+    name,
+    email,
+    phone,
+    suburb,
+    dogDetails,
+    details
+  })
+    .then(() => {
+      return res.sendStatus(200);
+    })
+    // .catch(err => console.log("&&&&&&&&&&&", err));
+    .catch(err => res.status(500).send(err));
 
-//enquires email sent to admin
-//  await transporter.sendMail({
-//     to: "josephine.yt.chong@gmail.com",
-//     from: email,
-//     subject: "New Enquiry/Comment",
-//     html: "<h1>An enquiry has been made please reply!</h1>",
-//     priority: "high"
-// });
-//enquires email sent to user
-// await transporter.sendMail({
-//     to: email,
-//     from: "dog@trainer.com",
-//     subject: "Enquiry/Comment has been Sent",
-//     html: "<h1>Your enquiry/comment has been sent!</h1>",
-//     priority: "high"
-// });
+  // enquires email sent to admin
+  await transporter.sendMail({
+    to: "josephine.yt.chong@gmail.com",
+    from: email,
+    subject: "New Enquiry/Comment",
+    html: "<h1>An enquiry has been made please reply!</h1>",
+    priority: "high"
+  });
+  // enquires email sent to user
+  await transporter.sendMail({
+    to: email,
+    from: "dog@trainer.com",
+    subject: "Enquiry/Comment has been Sent",
+    html: "<h1>Your enquiry/comment has been sent!</h1>",
+    priority: "high"
+  });
 
-//  console.log("Message sent", contact);
- // res.redirect("/contact");
- return res.sendStatus(404);
- return res.json(contact);
+  console.log("Message sent", contact);
+  res.redirect("/contacts");
+  return res.sendStatus(404);
+
+  return res.json(contact);
 }
 
 async function show(req, res) {
- const { id } = req.params;
- const contact = await ContactModel.findById(id);
- console.log(contact);
- return res.json(contact);
+  const { id } = req.params;
+  const contact = await ContactModel.findById(id);
+  console.log(contact);
+  return res.json(contact);
 }
 
 async function destroy(req, res) {
- let { id } = req.params;
- await ContactModel.findByIdAndRemove(id);
- res.redirect("/contact");
+  let { id } = req.params;
+  await ContactModel.findByIdAndRemove(id);
+  res.redirect("/contact");
 }
 
 async function update(req, res) {
- const { id } = req.params;
- const contact = await ContactModel.findById(id);
- return res.json(contact);
+  const { id } = req.params;
+  const contact = await ContactModel.findById(id);
+  return res.json(contact);
 }
 
 async function edit(req, res) {
- const { id } = req.params;
- let query = { _id: id };
- const contact = await ContactModel.update(query, {
-   $set: {
-     name,
-     email,
-     phone,
-     suburb,
-     dogDetails,
-     details
-   }
- });
+  const { id } = req.params;
+  let query = { _id: id };
+  const contact = await ContactModel.update(query, {
+    $set: {
+      name,
+      email,
+      phone,
+      suburb,
+      dogDetails,
+      details
+    }
+  });
 
- return res.json(contact);
+  return res.json(contact);
 }
 
 module.exports = {
- index,
- create,
- show,
- destroy,
- update,
- edit
+  index,
+  create,
+  show,
+  destroy,
+  update,
+  edit
 };
--- tests/integration/customers/create.test.js
+++ tests/integration/customers/create.test.js
@@ -1,23 +1,34 @@
 const mongoose = require("mongoose");
+const express = require("express");
 const supertest = require("supertest");
 const app = require("./../../../app");
 
-
 beforeAll(() => {
-    mongoose.connect("mongodb://localhost/deployment_test", { useNewUrlParser: true });
-    mongoose.Promise = global.Promise;
-    mongoose.connection.on("error", err => console.log(err));
+  mongoose.connect("mongodb://localhost/deployment_test", {
+    useNewUrlParser: true
+  });
+  mongoose.Promise = global.Promise;
+  mongoose.connection.on("error", err => console.log(err));
 });
-
 
 afterAll(() => {
-    console.log("aahhhhahahahhahhhh")
-    mongoose.connection.close();
+  console.log("aahhhhahahahhahhhh");
+  mongoose.connection.close();
 });
 
-
 describe("The user creates a new contact", () => {
-    it("POST /contactus with valid req body", () => {
-        expect(2).toEqual(2);
-    });
+  it("POST /contactus with valid req body", async () => {
+    const response = await supertest(app)
+      .post("/contactus")
+      .send({
+        name: "Nathaniel",
+        email: "test@test.com",
+        phone: 123456789,
+        suburb: "suburb",
+        dogDetails: "bacon",
+        details: "bacon detailss"
+      })
+      .expect(200);
+    expect(response.body).toEqual({});
+  });
 });
